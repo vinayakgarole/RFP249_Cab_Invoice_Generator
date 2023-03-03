076dfe96a1d07884d42681fa588cf5d62e040ccf
vinayakgarole <vinayakgarole10@gamil.com>
2023-03-03 19:51:03 +0530

    Modified: Added enhanced invoice

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -4,22 +4,25 @@ public class CabInvoiceGenerator {
 
     private static final int MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
+    private static final int MIN_FARE = 5;
 
-    //Creating calculateFare method to calculate the fare for the given distance and time
+    // Creating calculateFare method to calculate the fare for the given distance and time
     public static double calculateFare(double distance, int time) {//parameterized method
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MIN_FARE);
     }
 
-    //Creating calculateFare method with multiple rides
-    public double calculateFare(Ride[] rides) {
-        double totalFare = 0.0;
-        for (Ride ride : rides) {//Iterate over the rides
-            totalFare += calculateFare(ride.getDistance(), ride.getTime());//calculate total fare
+    // Creating calculateFare method with multiple rides
+    public Invoice calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
         }
-        return totalFare;
+        return new Invoice(rides.length, totalFare);
     }
 
     public static void main(String[] args) {
-        System.out.println("Total Fair is : " + calculateFare(5.5, 10));//calling method calculateFare and pass distance and time
+
+        System.out.println("Total Fair is : " + calculateFare(5.5, 10));   // Calling method calculateFare and pass distance and time
     }
 }
