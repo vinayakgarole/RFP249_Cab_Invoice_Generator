66e6acaf31df9ec2e040cb445c2ba7600b3bd803
vinayakgarole <vinayakgarole10@gamil.com>
2023-03-03 16:53:06 +0530

    Modified: Added multiple rides and min fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -5,18 +5,21 @@ public class CabInvoiceGenerator {
     private static final int MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
 
-    /*
-     * Creating calculateFare method to calculate the fare for the given distance and time
-     */
-    public static double calculateFare(double distance, int time) {     // Parameterized method
+    //Creating calculateFare method to calculate the fare for the given distance and time
+    public static double calculateFare(double distance, int time) {//parameterized method
         return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
     }
 
-    public static void main(String[] args) {
+    //Creating calculateFare method with multiple rides
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {//Iterate over the rides
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());//calculate total fare
+        }
+        return totalFare;
+    }
 
-        /*
-         * Calling method calculateFare and pass distance and time
-         */
-        System.out.println("Total Fair is: " + calculateFare(5.5, 10));
+    public static void main(String[] args) {
+        System.out.println("Total Fair is : " + calculateFare(5.5, 10));//calling method calculateFare and pass distance and time
     }
 }
---
+++ src/main/java/com/bridgelabz/Ride.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class Ride {
+
+    private double distance;
+    private int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
